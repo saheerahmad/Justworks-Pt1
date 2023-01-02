cd3c2122080635b95d94b2b07a820d1b514187d2
saheerahmad <ahmad69@purdue.edu>
2023-01-01 15:42:14 -1200

    cors

--- Backend/main.py
+++ Backend/main.py
@@ -15,13 +15,6 @@ origins = [
     "http://localhost:3000"
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/")
 def home():
